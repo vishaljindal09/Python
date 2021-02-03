a66b4caf25c347aa52749ac763de4a128a0adc05
vishaljindal09 <vishaljindal09@gmail.com>
2021-02-03 11:39:50 +0530

    Update array of integers nums and an integer target.py
    Brute force solution

--- LeetCode/Easy/array of integers nums and an integer target.py
+++ LeetCode/Easy/array of integers nums and an integer target.py
@@ -6,7 +6,23 @@ You may assume that each input would have exactly one solution, and you may not 
 You can return the answer in any order.
 
 """
+""" Brute force method
 
+Time complexity : O(n^2). For each element, we try to find its complement by looping through the rest of array which takes O(n)O(n) time.
+Therefore, the time complexity is O(n^2).
+
+Space complexity : O(1).
+
+"""
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
+        for i in range (0, len(nums)):
+            current_value = nums[i]
+            
+            for j in range (i+1, len(nums)):
+                value_needed = target - current_value
+                
+                if nums[j] == value_needed:
+                    result = [i,j]
+                    
+        return result
